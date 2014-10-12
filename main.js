dce2f4fd1f393035f3d17a9e89712a39340f01e7
Benjamin Hoffman <Benjamin@Benjamins-MacBook-Air.local>
2014-10-11 21:35:51 -0700

    uploading my alarm clock

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-console.log('my name is ben')
+
+
+$(document).ready(function(){
+	$( "#submit" ).on("click", reactToClick);
+
+	function reactToClick(){
+
+		console.log( "You submitted!" );
+
+		var userAnswer = $('#time').val()
+
+		console.log(userAnswer)
+
+		setTimeout(function() {
+		
+			console.log("alarm bitches")
+
+
+		}, userAnswer);
+
+		var currentTime = userAnswer
+
+		setInterval(function() {
+
+			currentTime = currentTime - 1000;
+
+			console.log(currentTime)
+
+		}, 1000);
+	}
+});
